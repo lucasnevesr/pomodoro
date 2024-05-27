65ed9da468e97cdfff07530ff10a1fc77713e4e3
Lucas Neves <nevessricardoo@gmail.com>
2024-05-27 17:14:52 -0300

    Add break logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 import Buttons from './components/Buttons/Buttons';
 
 function App() {
+  //controlar se o que esta rodando é o break ou o pomodoro
   const [breakLength, setBreakLength] = useState(5);
+  const [nextPeriod, setNextPeriod] = useState(breakLength);
   const [pomoLenght, setPomoLength] = useState(25);
   const [secondsLeft, setSecondsLeft] = useState(pomoLenght * 60);
   const [isRunning, setIsRunning] = useState(false);
@@ -17,15 +19,26 @@ function App() {
       if (secondsLeft === 0) {
         clearInterval(interval);
         setIsRunning(false);
-        //avisar que acabou o tempo e irá começar a outra etapa (break ou pomodoro)
-        //controlar se o que esta rodando é o break ou o pomodoro
-        //setar o secondsLeft para a etapa definida
-        // se for o pomodoro, não começar automático
+
+        if (nextPeriod === breakLength) {
+          //avisar que acabou o tempo e irá começar a outra etapa (break ou pomodoro)
+          alert('Break start now!');
+          //setar o secondsLeft para a etapa definida
+          setSecondsLeft(breakLength * 60);
+          setIsRunning(true);
+          setNextPeriod(pomoLenght);
+        } else if (nextPeriod === pomoLenght) {
+          alert('Break ended! Pomodoro is will be restarted now.');
+          // se for o pomodoro, não começar automático
+          setIsRunning(false);
+          setSecondsLeft(pomoLenght * 60);
+          setNextPeriod(breakLength);
+        }
       }
 
       return () => clearInterval(interval);
     }
-  }, [isRunning, secondsLeft]);
+  }, [isRunning, secondsLeft, pomoLenght, breakLength, nextPeriod]);
 
   const formatTimeLeft = (seconds) => {
     return `${Math.floor(seconds / 60)} : ${seconds % 60 > 9 ? seconds % 60 : '0' + (seconds % 60)}`;
